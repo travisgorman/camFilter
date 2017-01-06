c2ba77b173fba2f06ed5f717985c83b145e14298
Travis Gorman <thefarbetween@me.com>
2017-01-06 15:03:51 -0600

    rgbSplit filter

--- scripts.js
+++ scripts.js
@@ -9,9 +9,6 @@ function getVideo() {
 		video: true,
 		audio: false 
 	}).then(localMediaStream => {
-
-		console.log( localMediaStream );
-
 		video.src = window.URL
 			.createObjectURL(localMediaStream);
 			video.play();
@@ -22,35 +19,56 @@ function getVideo() {
 }
 
 function paintToCanvas() {
-	const width = video.videoWidth;
-	const height = video.videoHeight;
-	canvas.width = width;
-	canvas.height = height;
-
-	setInterval(() => {
-		ctx.drawImage(video, 0,0, width, height);
-	}, 16);
-
-	//
-
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+  return setInterval(() => {
+  	ctx.drawImage(video, 0, 0, width, height);
+  	let pixels = ctx.getImageData(0,0,width, height);
+  	// pixels = redEffect(pixels);
+  	pixels = rgbSplit(pixels);
+  	ctx.putImageData(pixels, 0,0);
+  }, 16);
 }
 
-function takePhoto() {
+function redEffect(pixels) {
+	  for(let i = 0; i < pixels.data.length; i+=4) {
+		  pixels.data[i + 0] = pixels.data[i + 0] + 100;
+	    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+	    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+	 }
+	 return pixels;
+}
 
+function rgbSplit(pixels) {
+	for(let i = 0; i < pixels.data.length; i+=4) {
+	  pixels.data[i - 50] = pixels.data[i + 0] + 100;
+    pixels.data[i + 200] = pixels.data[i + 1] - 50;
+    pixels.data[i - 550] = pixels.data[i + 2] * 0.5;
+	 }
+	 return pixels;	
+}
+
+
+function takePhoto() {
 	snap.currentTime = 0;
 	snap.play();
-
 	const data = canvas.toDataURL('image/jpeg');
 	const link = document.createElement('a');
 	link.href = data;
 	link.setAttribute('download', 'handsome');
-	// link.textContent = 'Download Image';
 	link.innerHTML = `<img src="${data}" alt="handsome man"/>`;
 	strip.insertBefore(link, strip.firstChild);
-
-console.log( link )
-
 }
+
+
+
+
+
+
+
+
 
 getVideo();
 
